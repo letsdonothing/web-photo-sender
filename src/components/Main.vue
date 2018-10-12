c8bfaff71e7c92dfcd8b00b78d3898691ad5285e
Sergey Novikov <33960459+letsdonothing@users.noreply.github.com>
2018-10-12 19:43:02 +0300

    set button position and fix error from other than iOS Safari browser

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -14,11 +14,16 @@ export default {
   data () {
     return {
       video: null,
-      paused: false
+      paused: false,
+      readyToMove: null
     }
   },
   mounted() {
-  //  const isSafari = !!navigator.userAgent.match(/Version\/[\d\.]+.*Safari/)
+    const isSafari = !!navigator.userAgent.match(/Version\/[\d\.]+.*Safari/);
+    const isIos = !!navigator.userAgent.match(/iPad/i) || !!navigator.userAgent.match(/iPhone/i);
+
+    this.readyToMove = isSafari & isIos;
+
     this.startStream();
   },
   methods: {
@@ -26,16 +31,18 @@ export default {
       this.video.pause();
       this.paused = true;
 
-      const canvas = document.querySelector('canvas');
-      const context = canvas.getContext('2d');
+      if (this.readyToMove) {
+        const canvas = document.querySelector('canvas');
+        const context = canvas.getContext('2d');
 
-      canvas.width = this.video.videoWidth;
-      canvas.height = this.video.videoHeight;
+        canvas.width = this.video.videoWidth;
+        canvas.height = this.video.videoHeight;
 
-      context.drawImage(this.video, 0, 0, canvas.width, canvas.height);
-      const imgSrc = canvas.toDataURL('image/png');
+        context.drawImage(this.video, 0, 0, canvas.width, canvas.height);
+        const imgSrc = canvas.toDataURL('image/png');
 
-      window.location = 'image-receiver://' + imgSrc.slice(22);
+        window.location = 'image-receiver://' + imgSrc.slice(22);
+      }
     },
     startStream() {
       this.video = document.querySelector('video');
@@ -73,24 +80,35 @@ export default {
 button {
   position: absolute;
   z-index: 2;
-  top: 110vw;
+  top: calc(133.3vw - 64px);
   left: calc(50vw - 50px);
   width: 100px;
   height: 44px;
   border: solid 1px black;
   border-radius: 22px;
-  background-color: white
-
+  background-color: white;
+  outline: none;
 }
 video {
-  position: relative;
+  position: absolute;
   z-index: 1;
+  left: 0;
   width: 100vw;
   height: 133.3vw;
   background-color: #ccc;
 }
 canvas {
   display: none;
+}
+
+@media all and (orientation: landscape) {
+  video {
+    height: 100vh;
+  }
+  button {
+    left: calc(100vw - 130px);
+    top: calc(50vh - 22px);
+  }
 }
 
 </style>
